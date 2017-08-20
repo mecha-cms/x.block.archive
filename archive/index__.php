96987934dfba0af6552a8166072e9a1f41a2451c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-08-20 17:48:38 +0700

    Rename Function

--- archive/index__.php
+++ archive/index__.php
@@ -1,6 +1,6 @@
 <?php
 
-function fn_archive($path = PAGE) {
+function fn_archive_html($path = PAGE) {
     $html = "";
     if (!Is::D($path)) {
         return $html;
@@ -24,7 +24,7 @@ function fn_archive($path = PAGE) {
             } else {
                 $html .= '<a href="' . URL::short($u, false) . '">' . $t . '</a>';
             }
-            $html .= fn_archive($x . DS . $y);
+            $html .= fn_archive_html($x . DS . $y);
             $html .= '</li>';
         }
         $html .= '</ul>';
@@ -32,18 +32,18 @@ function fn_archive($path = PAGE) {
     return $html;
 }
 
-function fn_archive_replace_archive($content) {
+function fn_archive($content) {
     global $url;
     return Block::replace('archive', function() use($url) {
         $cache = str_replace(ROOT, CACHE, __DIR__) . '.php';
         $x = File::open($cache)->import(["", ""]);
-        $hash = md5(json_encode(File::explore(PAGE, true, true)));
+        $hash = md5(json_encode(File::explore(PAGE, true)));
         if ($x[0] !== $hash) {
-            $x = [$hash, fn_archive()];
+            $x = [$hash, fn_archive_html()];
             File::export($x)->saveTo($cache);
         }
         return str_replace(' href="', ' href="' . $url . '/', $x[1]);
     }, $content);
 }
 
-Block::set('archive', 'fn_archive_replace_archive', 10);
+Block::set('archive', 'fn_archive', 10);
