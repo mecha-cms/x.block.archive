12100e73451f71404afc8891b2233cf66bc7010c
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-02-04 22:21:47 +0700

    `Cache::hit()` and `Cache::alt()`

--- archive/index.php
+++ archive/index.php
@@ -1,12 +1,12 @@
 <?php
 
 namespace fn {
-    function archive(string $path) {
+    function archive(string $path, $date, $i, $level) {
         $out = "";
-        if (!\is_dir($path)) {
+        if (!\is_dir($path) || $level < 1) {
             return $out;
         }
-        $files = \Get::pages($path, 'page', [1, 'title'], 'path');
+        $files = \Get::pages($path, 'page', $date ? [-1, 'time'] : [1, 'title'], 'path');
         if ($files->count()) {
             $out .= '<ul>';
             foreach ($files as $file) {
@@ -16,14 +16,14 @@ namespace fn {
                 $page = new \Page($file);
                 $url = $page->url;
                 $title = $page->title;
-                $current = $GLOBALS['URL']['clean'] === $url;
-                $out .= '<li' . ($current ? ' class="current"' : "") . '>';
-                if ($current) {
-                    $out .= '<span>' . $title . '</span>';
-                } else {
-                    $out .= '<a href="' . \URL::short($url, false) . '">' . $title . '</a>';
+                $out .= '<li>';
+                if ($date) {
+                    $out .= '<time datetime="' . $page->time->ISO8601 . '">' . $page->time($date) . '</time>&#x2003;';
                 }
-                $out .= archive(\Path::F($file));
+                $out .= '<a href="' . \URL::short($url, false) . '">' . $title . '</a>';
+                if ($i < $level) {
+                    $out .= archive(\Path::F($file), $date, $i + 1, $level);
+                }
                 $out .= '</li>';
             }
             $out .= '</ul>';
@@ -33,12 +33,19 @@ namespace fn {
 }
 
 namespace fn\block {
-    function archive($a, $b) {
-        $path = \rtrim(PAGE . DS . \strtr($b['path'] ?? "", '/', DS), DS);
-        $cache = \Cache::expire($path) ? \Cache::set($path, function() use($path) {
-            return \fn\archive($path);
-        }) : \Cache::get($path, "");
-        return \str_replace(' href="', ' href="' . $GLOBALS['URL']['$'] . '/', $cache);
+    function archive($content, $attr) {
+        extract(\extend([
+            'date' => false,
+            'level' => 5,
+            'path' => ""
+        ], $attr), \EXTR_SKIP);
+        // Refresh cache by adding `?cache=0` or `?cache=false` to the current URL
+        $expire = \HTTP::is('get', 'cache') && !\HTTP::get('cache') ? 0 : '1 year';
+        $path = \rtrim(PAGE . DS . \strtr($path ?? "", '/', DS), DS);
+        $content = \Cache::alt($path . \json_encode($attr), function() use($date, $level, $path) {
+            return \fn\archive($path, $date && !\is_string($date) ? '%Y%.%M%.%D%' : $date, 1, $level);
+        }, $expire) ?? "";
+        return \str_replace(' href="', ' href="' . $GLOBALS['URL']['$'] . '/', $content);
     }
     \Block::set('archive', __NAMESPACE__ . "\\archive", 10);
 }
