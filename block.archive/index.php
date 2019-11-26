66f9ed6b219414213424d17f0aa55cb7de1ff9bd
Taufik Nurrohman <t.nurrohman77@gmail.com>
2019-11-26 23:30:12 +0700

    Remove Automatic Constant Feature

--- block.archive/index.php
+++ block.archive/index.php
@@ -1,24 +1,24 @@
 <?php
 
 namespace _\lot\x\block\archive {
-    function content(string $path, $date, $i, $deep) {
+    function content(string $path, $time, $i, $deep) {
         $out = "";
         if (!\is_dir($path)) {
             return $out;
         }
-        $pages = \Pages::from($path)->sort($date ? [-1, 'time'] : [1, 'title']);
+        $pages = \Pages::from($path)->sort($time ? [-1, 'time'] : [1, 'title']);
         if ($pages->count()) {
             $out .= '<ul>';
             foreach ($pages as $page) {
                 $url = $page->url;
                 $title = $page->title;
                 $out .= '<li>';
-                if ($date) {
-                    $out .= '<time datetime="' . $page->time->ISO8601 . '">' . $page->time($date) . '</time>&#x2003;';
+                if ($time) {
+                    $out .= '<time datetime="' . $page->time->ISO8601 . '">' . $page->time($time) . '</time>&#x2003;';
                 }
                 $out .= '<a href="' . \URL::short($url, false) . '">' . $title . '</a>';
                 if ($i < $deep) {
-                    $out .= content(\Path::F($page->path), $date, $i + 1, $deep);
+                    $out .= content(\Path::F($page->path), $time, $i + 1, $deep);
                 }
                 $out .= '</li>';
             }
@@ -31,15 +31,15 @@ namespace _\lot\x\block\archive {
 namespace _\lot\x\block {
     function archive($content, $attr) {
         extract(\array_replace([
-            'date' => false,
             'deep' => 4,
-            'path' => ""
+            'path' => "",
+            'time' => false
         ], $attr), \EXTR_SKIP);
-        // Refresh cache by adding `?cache=0` or `?cache=false` to the current URL
-        $expire = \Request::is('get', 'cache') && !\Get::get('cache') ? 0 : '1 year';
-        $path = \rtrim(\PAGE . \DS . \strtr($path ?? "", '/', \DS), \DS);
-        $content = \Cache::live($path . \json_encode($attr), function() use($date, $deep, $path) {
-            return \_\lot\x\block\archive\content($path, $date && !\is_string($date) ? '%Y.%m.%d' : $date, 0, $deep);
+        // Refresh cache by appending `?cache=0` or `?cache=false` to the current URL
+        $expire = \Request::is('Get', 'cache') && !\Get::get('cache') ? 0 : '1 year';
+        $path = \rtrim(\LOT . \DS . 'page' . \strtr($path ?? "", '/', \DS), \DS);
+        $content = \Cache::live($path . \json_encode($attr), function() use($time, $deep, $path) {
+            return \_\lot\x\block\archive\content($path, $time && !\is_string($time) ? '%Y.%m.%d' : $time, 0, $deep);
         }, $expire) ?? "";
         return \str_replace(' href="', ' href="' . $GLOBALS['url'] . '/', $content);
     }
